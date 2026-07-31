d4176e2a049233476f52cff9587720891745d74d
dverma9165 <dverma9165@gmail.com>
2026-07-31 17:25:04 +0530

    inventory added

--- src/components/element/DataTable.tsx
+++ src/components/element/DataTable.tsx
@@ -114,7 +114,7 @@ export default function DataTable<TData, TValue>({
                                             <TableHead
                                                 key={header.id}
                                                 className={cn(
-                                                    "h-10 px-2 align-middle font-medium text-gray-900 border-b",
+                                                    "sticky top-0 z-10 bg-muted h-10 px-2 align-middle font-medium text-gray-900 border-b",
                                                     header.column.getCanSort() ? "cursor-pointer select-none group" : ""
                                                 )}
                                                 onClick={header.column.getToggleSortingHandler()}
